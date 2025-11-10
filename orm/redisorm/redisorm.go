6eee5ddefb4bcd096fd7c244b80b489749df0917
Jaeun-Choi98 <wodns617@gmail.com>
2025-11-10 17:42:38 +0900

    add redis orm module

---
+++ orm/redisorm/redisorm.go
@@ -0,0 +1,302 @@
+package redisorm
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	redis "github.com/redis/go-redis/v9"
+)
+
+type Model interface {
+	GetId() int64
+	SetId(id int64)
+	TableName() string
+	GetIndexFields() map[string]any
+}
+
+type RedisClient struct {
+	rdb     *redis.Client
+	mu      sync.RWMutex
+	ctx     context.Context
+	timeout time.Duration
+}
+
+func NewRedisClient(addr, passwd string, db, protocol, timeout int) *RedisClient {
+	new := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: passwd,
+		DB:       db,
+		Protocol: protocol,
+	})
+
+	return &RedisClient{
+		rdb:     new,
+		ctx:     context.Background(),
+		timeout: time.Duration(timeout) * time.Second,
+	}
+}
+
+func (c *RedisClient) Close() error {
+	return c.rdb.Close()
+}
+
+type Repository[T Model] struct {
+	client      *RedisClient
+	tableName   string
+	indexFields map[string]any
+}
+
+func NewRepository[T Model](c *RedisClient, m T) *Repository[T] {
+	return &Repository[T]{
+		client:      c,
+		tableName:   m.TableName(),
+		indexFields: m.GetIndexFields(),
+	}
+}
+
+func (r *Repository[T]) Create(model T) error {
+
+	r.client.mu.Lock()
+	defer r.client.mu.Unlock()
+
+	timeoutCtx, cancel := context.WithTimeout(r.client.ctx, r.client.timeout)
+	defer cancel()
+
+	if err := r.validate(model); err != nil {
+		return err
+	}
+
+	indexes := model.GetIndexFields()
+	for field, value := range indexes {
+		if exists, _ := r.checkDuplicate(field, value); exists {
+			return fmt.Errorf("duplicate %s: %v", field, value)
+		}
+	}
+
+	idKey := fmt.Sprintf("next_%s_id", r.tableName)
+	id, err := r.client.rdb.Incr(timeoutCtx, idKey).Result()
+	if err != nil {
+		return err
+	}
+
+	model.SetId(id)
+
+	jsonData, err := json.Marshal(model)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.client.rdb.TxPipelined(timeoutCtx, func(pipe redis.Pipeliner) error {
+		primaryKey := fmt.Sprintf("%s:%d", r.tableName, id)
+		pipe.Do(timeoutCtx, "JSON.SET", primaryKey, "$", string(jsonData))
+
+		for field, value := range indexes {
+			indexKey := fmt.Sprintf("%s:idx:%s", r.tableName, field)
+			pipe.HSet(timeoutCtx, indexKey, fmt.Sprint(value), id)
+		}
+
+		allIdsKey := fmt.Sprintf("%s:all_ids", r.tableName)
+		pipe.SAdd(timeoutCtx, allIdsKey, id)
+
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to create: %w", err)
+	}
+	return err
+}
+
+func (r *Repository[T]) FindById(id int64) (T, error) {
+	r.client.mu.RLock()
+	defer r.client.mu.RUnlock()
+
+	timeoutCtx, cancel := context.WithTimeout(r.client.ctx, r.client.timeout)
+	defer cancel()
+
+	var model T
+
+	primaryKey := fmt.Sprintf("%s:%d", r.tableName, id)
+
+	result, err := r.client.rdb.Do(timeoutCtx, "JSON.GET", primaryKey, "$").Result()
+	if err != nil {
+		return model, fmt.Errorf("not exists record: %v", err)
+	}
+
+	jsonStr, ok := result.(string)
+	if !ok {
+		return model, fmt.Errorf("invalid JSON response")
+	}
+
+	var jsonArray []json.RawMessage
+	if err := json.Unmarshal([]byte(jsonStr), &jsonArray); err != nil {
+		return model, err
+	}
+
+	if len(jsonArray) == 0 {
+		return model, fmt.Errorf("record not found")
+	}
+
+	if err := json.Unmarshal(jsonArray[0], &model); err != nil {
+		return model, err
+	}
+
+	return model, nil
+}
+
+func (r *Repository[T]) FindByIndex(fieldName string, value interface{}) (T, error) {
+	r.client.mu.RLock()
+
+	timeoutCtx, cancel := context.WithTimeout(r.client.ctx, r.client.timeout)
+	defer cancel()
+
+	var model T
+
+	indexKey := fmt.Sprintf("%s:idx:%s", r.tableName, fieldName)
+	idStr, err := r.client.rdb.HGet(timeoutCtx, indexKey, fmt.Sprint(value)).Result()
+	if err != nil {
+		return model, fmt.Errorf("record not found: %w", err)
+	}
+
+	var id int64
+	fmt.Sscanf(idStr, "%d", &id)
+
+	r.client.mu.RUnlock()
+
+	return r.FindById(id)
+}
+
+func (r *Repository[T]) FindAll() ([]T, error) {
+	r.client.mu.RLock()
+
+	timeoutCtx, cancel := context.WithTimeout(r.client.ctx, r.client.timeout)
+	defer cancel()
+
+	allIdsKey := fmt.Sprintf("%s:all_ids", r.tableName)
+	idStrs, err := r.client.rdb.SMembers(timeoutCtx, allIdsKey).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	r.client.mu.RUnlock()
+
+	models := make([]T, 0, len(idStrs))
+	for _, idStr := range idStrs {
+		var id int64
+		fmt.Sscanf(idStr, "%d", &id)
+
+		model, err := r.FindById(id)
+		if err != nil {
+			continue
+		}
+		models = append(models, model)
+	}
+
+	return models, nil
+}
+
+func (r *Repository[T]) Update(model T) error {
+
+	timeoutCtx, cancel := context.WithTimeout(r.client.ctx, r.client.timeout)
+	defer cancel()
+
+	id := model.GetId()
+	if id == 0 {
+		return fmt.Errorf("model Id is required for update")
+	}
+
+	oldModel, err := r.FindById(id)
+	if err != nil {
+		return fmt.Errorf("record not found: %w", err)
+	}
+
+	r.client.mu.Lock()
+	defer r.client.mu.Unlock()
+
+	jsonData, err := json.Marshal(model)
+	if err != nil {
+		return fmt.Errorf("failed to marshal model: %w", err)
+	}
+
+	_, err = r.client.rdb.TxPipelined(timeoutCtx, func(pipe redis.Pipeliner) error {
+		primaryKey := fmt.Sprintf("%s:%d", r.tableName, id)
+		pipe.Do(timeoutCtx, "JSON.SET", primaryKey, "$", string(jsonData))
+
+		oldIndexes := oldModel.GetIndexFields()
+		for field, value := range oldIndexes {
+			indexKey := fmt.Sprintf("%s:idx:%s", r.tableName, field)
+			pipe.HDel(timeoutCtx, indexKey, fmt.Sprint(value))
+		}
+
+		newIndexes := model.GetIndexFields()
+		for field, value := range newIndexes {
+			indexKey := fmt.Sprintf("%s:idx:%s", r.tableName, field)
+			pipe.HSet(timeoutCtx, indexKey, fmt.Sprint(value), id)
+		}
+
+		_, err = pipe.Exec(timeoutCtx)
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to update: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository[T]) Delete(id int64) error {
+
+	model, err := r.FindById(id)
+	if err != nil {
+		return err
+	}
+
+	r.client.mu.Lock()
+	defer r.client.mu.Unlock()
+
+	timeoutCtx, cancel := context.WithTimeout(r.client.ctx, r.client.timeout)
+	defer cancel()
+
+	_, err = r.client.rdb.TxPipelined(timeoutCtx, func(pipe redis.Pipeliner) error {
+
+		primaryKey := fmt.Sprintf("%s:%d", r.tableName, id)
+		pipe.Del(r.client.ctx, primaryKey)
+
+		indexes := model.GetIndexFields()
+		for field, value := range indexes {
+			indexKey := fmt.Sprintf("%s:idx:%s", r.tableName, field)
+			pipe.HDel(r.client.ctx, indexKey, fmt.Sprint(value))
+		}
+
+		allIDsKey := fmt.Sprintf("%s:all_ids", r.tableName)
+		pipe.SRem(r.client.ctx, allIDsKey, id)
+
+		_, err = pipe.Exec(r.client.ctx)
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to delete: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository[T]) validate(model T) error {
+	indexes := model.GetIndexFields()
+	for field, value := range indexes {
+		if value == nil || fmt.Sprint(value) == "" {
+			return fmt.Errorf("index field %s cannot be empty", field)
+		}
+	}
+	return nil
+}
+
+func (r *Repository[T]) checkDuplicate(field string, value interface{}) (bool, error) {
+	indexKey := fmt.Sprintf("%s:idx:%s", r.tableName, field)
+	return r.client.rdb.HExists(r.client.ctx, indexKey, fmt.Sprint(value)).Result()
+}
---
+++ orm/redisorm/test/redisorm_test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Jaeun-Choi98/modules/orm/redisorm"
+	"github.com/stretchr/testify/assert"
+)
+
+var addr = ``
+var psssword = ``
+
+// Device 모델
+type Device struct {
+	Id           int64             `json:"id"`
+	SerialNumber string            `json:"serial_number"`
+	Model        string            `json:"model"`
+	Status       string            `json:"status"`
+	Power        float64           `json:"power"`
+	Specs        map[string]string `json:"specs"` // 복잡한 구조도 JSON으로 쉽게!
+	Tags         []string          `json:"tags"`
+}
+
+func (d *Device) GetId() int64      { return d.Id }
+func (d *Device) SetId(id int64)    { d.Id = id }
+func (d *Device) TableName() string { return "devices" }
+func (d *Device) GetIndexFields() map[string]interface{} {
+	return map[string]interface{}{
+		"serial_number": d.SerialNumber,
+		"status":        d.Status,
+	}
+}
+func TestRedisORMCreateAndFind(t *testing.T) {
+	redisClient := redisorm.NewRedisClient(addr, psssword, 0, 2, 5)
+	dvcRepository := redisorm.NewRepository(redisClient, &Device{})
+	dvc1 := &Device{
+		SerialNumber: "1asd32d213",
+		Model:        "test model",
+		Status:       "test status",
+		Power:        324.512,
+		Specs:        map[string]string{"spec1": "sdfds", "spec2": "sdfdsdsf"},
+		Tags:         []string{"tag1", "tag2"},
+	}
+	err := dvcRepository.Create(dvc1)
+	assert.NoError(t, err, "")
+	t.Log(dvcRepository.FindAll())
+	val, _ := dvcRepository.FindByIndex("serial_number", dvc1.SerialNumber)
+	t.Log(val)
+}
+
+func TestRedisORMUpdate(t *testing.T) {
+	redisClient := redisorm.NewRedisClient(addr, psssword, 0, 2, 5)
+	dvcRepository := redisorm.NewRepository(redisClient, &Device{})
+	dvc1, _ := dvcRepository.FindByIndex("serial_number", "1asd32d213")
+	t.Logf("%v", dvc1)
+	dvc1.SerialNumber = "update serial number"
+	dvc1.Specs = map[string]string{"spec1": "update spec1", "spec2": "update spec2"}
+	err := dvcRepository.Update(dvc1)
+	assert.NoError(t, err)
+}
+
+func TestRedisORMDelete(t *testing.T) {
+	redisClient := redisorm.NewRedisClient(addr, psssword, 0, 2, 5)
+	dvcRepository := redisorm.NewRepository(redisClient, &Device{})
+	dvc1, _ := dvcRepository.FindByIndex("serial_number", "update serial number")
+	err := dvcRepository.Delete(dvc1.Id)
+	assert.NoError(t, err)
+}
